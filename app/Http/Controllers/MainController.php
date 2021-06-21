631e446bf0bd739a22b0e3122225e27f4045ed46
Oriane-Toque <oriane.toque@gmail.com>
2021-06-21 12:25:51 +0200

    cours + correction

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categories;
+use Illuminate\Support\Facades\DB;
+
 class MainController extends Controller{
 
     public function home()
     {
         echo "YATA MEP 1ere route avec LUMEN";
+
+        // $results = DB::select('SELECT * FROM `tasks`');
+        // dump($results);
+
+        $categories = new Categories;
+        $results = $categories::all();
+        dump($results);
     }
 
 }
